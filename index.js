8695a6f64673869c0b124002a5f3720100c07a3d
Abdelaziz <fazouz2010@gmail.com>
2023-01-11 23:12:30 +0300

    Added comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Function for checking if the input is an alphabet, returns boolean.
 function isAlphabet(str) {
   return /^[a-zA-Z()]+$/.test(str);
 }
@@ -6,21 +7,23 @@ const caesar = (plainText, key) => {
   const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let cipherText = "";
 
+  // Iterate over each character in plainText string.
   for (let i = 0; i < plainText.length; i++) {
+    // Check if the ith character is an alphabet.
     if (isAlphabet(plainText[i])) {
+      /* Iterate over each character in the alphabet and compare it
+			with ith character in plaintext if matched enycrpt the letter. */
       for (let j = 0; j < alphabets.length; j++) {
         if (plainText[i].toUpperCase() === alphabets[j].toUpperCase()) {
           cipherText = cipherText + alphabets[(j + key) % 26].toUpperCase();
-
           break;
         }
       }
+      // If not an alphabet store it as it is.
     } else {
       cipherText = cipherText + plainText[i];
     }
   }
-
-  console.log(cipherText);
   return cipherText;
 };
 
